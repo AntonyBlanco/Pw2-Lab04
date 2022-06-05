3948d3d040de3a5d5bee8df7f0ef2205066a9077
JessHV <jhanccove@unsa.edu.pe>
2022-06-05 11:06:33 -0500

    correcciones return

--- Tarea-del-Ajedrez/picture.py
+++ Tarea-del-Ajedrez/picture.py
@@ -16,7 +16,7 @@ class Picture:
     vertical = []
     for value in self.img:
       vertical.append(value[::-1])
-    return vertical
+    return Picture(vertical)
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
@@ -24,7 +24,7 @@ class Picture:
     self.img.reverse()
     for value in self.img:
       horizontal.append(value[:])
-    return horizontal
+    return Picture(horizontal)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
