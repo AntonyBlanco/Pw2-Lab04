a7d6db00a9a1bfc3cd5aa1294c43036d9148fd1a
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-05 23:27:00 -0500

    Resolvi el ejercicio de los caballos volteados

--- Tarea-del-Ajedrez/Ejercicio2b.py
+++ Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+cabBlanVol = knight.verticalMirror()
+cabNegrVol = knight.negative().verticalMirror()
+fil1 = knight.join(knight.negative())
+fil2 = cabNegrVol.join(cabBlanVol)
+caballos = fil2.up(fil1)
+draw(caballos)
