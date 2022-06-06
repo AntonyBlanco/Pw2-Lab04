dc59825f739ae989dfb1ef342b3f970b1df591d5
oliver092377 <omayta@unsa.edu.pe>
2022-06-05 22:47:05 -0500

    cambios finales

--- Tarea-del-Ajedrez/Ejercicio2f.py
+++ Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,4 @@
 from interpreter import draw
 from chessPictures import *
+superior=square.join(square.negative()).horizontalRepeat(2)
+draw(superior)
--- Tarea-del-Ajedrez/picture.py
+++ Tarea-del-Ajedrez/picture.py
@@ -16,32 +16,17 @@ class Picture:
   def verticalMirror(self):
     """ Devuelve el espejo vertical de la imagen """
     content = []
-    for x in self.img:
-      content.append(x[::-1])
     return Picture(content)
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
     content = []
-    indice=len(self.img)-1
-    for x in self.img:
-      content.append(self.img[indice])
-      indice=indice-1
     return Picture(content)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
     content = []
-    x = 0
-    while x < len(self.img):
-      content.append("")
-      y = 0
-      while y < len(self.img[x]):
-        content[x] += self._invColor(self.img[x][y])
-        y += 1
-      x += 1
     return Picture(content)   
-  #yo mero :v
   def join(self, p):
     count = 0
     content = []
@@ -50,9 +35,9 @@ class Picture:
     for y in p.img:
       content[count] += y
       count = count+1
-    return Picture(content)
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
+    return Picture(content)
   def up(self, p):
     content = p.img + self.img
     return Picture(content)
@@ -60,32 +45,17 @@ class Picture:
   def under(self, p):
     """ Devuelve una nueva figura poniendo la figura p sobre la
         figura actual """
-    content = self.img + p.img
+    content = []
     return Picture(content)
   
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
     content=[]
-    for x in self.img:
-      content.append("")
-    count=0
-    while(n>0): 
-      content[count]+=self.img[count]
-      count += 1
-      if count==len(self.img):
-        print("len: ",len(self.img))
-        n=n-1
-        count=0
     return Picture(content)  
 
   def verticalRepeat(self, n):
     content=[]
-    for x in self.img:
-      content.append(x)
-    while(n-1>0): 
-      content+=self.img
-      n=n-1
     return Picture(content)   
 
   #Extra: Sólo para realmente viciosos 
